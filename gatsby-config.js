3860a7c2b0b5ad0a5ceb71399bcb7ad6b0a29174
junsun <groupjunsun@gmail.com>
2023-06-14 17:10:05 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `test-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
